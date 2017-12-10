2de853b95aa808003e94c3a58e4da900918446e3
reima4038 <reima4038@gmail.com>
2017-12-10 11:09:57 +0900

    initial commit

---
+++ src/main/java/org/jp/reima/form_matchup_table/FormMatchupTableApplication.java
@@ -0,0 +1,12 @@
+package org.jp.reima.form_matchup_table;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormMatchupTableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormMatchupTableApplication.class, args);
+	}
+}
