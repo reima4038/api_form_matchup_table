03dd47cb5666e6199e27ddc6288d3da2ede4cb50
reima4038 <reima4038@gmail.com>
2017-12-10 13:06:16 +0900

    コントローラーとサービスにアノテーションを付与

--- src/main/java/org/jp/reima/form_matchup_table/controller/MatchupTableRestController.java
+++ src/main/java/org/jp/reima/form_matchup_table/controller/MatchupTableRestController.java
@@ -1,5 +1,8 @@
 package org.jp.reima.form_matchup_table.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MatchupTableRestController {
 
 }
--- src/main/java/org/jp/reima/form_matchup_table/service/MatchupTableService.java
+++ src/main/java/org/jp/reima/form_matchup_table/service/MatchupTableService.java
@@ -1,5 +1,8 @@
 package org.jp.reima.form_matchup_table.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class MatchupTableService {
 
 }
