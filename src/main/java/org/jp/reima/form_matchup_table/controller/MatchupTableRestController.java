25195f7df8445c2c91f1624ac884ddd310ccc16c
reima4038 <reima4038@gmail.com>
2017-12-10 13:14:04 +0900

    YAGNI. Serviceクラスは後で必要になったら追加する。

--- src/main/java/org/jp/reima/form_matchup_table/controller/MatchupTableRestController.java
+++ src/main/java/org/jp/reima/form_matchup_table/controller/MatchupTableRestController.java
@@ -1,8 +1,56 @@
 package org.jp.reima.form_matchup_table.controller;
 
+import org.jp.reima.form_matchup_table.api.MatchupTableApi;
+import org.jp.reima.form_matchup_table.domain.repository.MatchRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class MatchupTableRestController {
+public class MatchupTableRestController implements MatchupTableApi {
+    
+    @Autowired
+    MatchRepository repos;
+    
+    @Override
+    public ResponseEntity<String> createMatch() {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<String> joinMatch(String matcheId, String memberName) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<String> leaveMatch(String matcheId, String memberName) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<String> findMatch(String matcheId) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<String> assignTeam(String matcheId) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<String> showTeams(String matcheId) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void startMatch(String matcheId) {
+        // TODO Auto-generated method stub
+        
+    }
 
 }
--- src/main/java/org/jp/reima/form_matchup_table/service/MatchupTableService.java
+++
@@ -1,8 +0,0 @@
-package org.jp.reima.form_matchup_table.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class MatchupTableService {
-
-}
